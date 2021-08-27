eb307ce1909f59f26fceb11f773a9f6419131c39
Gabriel Franco da Rocha Nascimento <gabriel.nascimento@sicoob.com.br>
2021-08-27 00:46:17 -0300

    Manipulação de Arquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,19 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.createReadStream('./assets/fluig.png')
-    .pipe(fs.createWriteStream('./assets/fluig-stream.png'))
-    .on('finish', () => console.log('Imagem foi escrita com sucesso!'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const tipoValidos = ['jpg','png','jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tipoValidos.indexOf(tipo.substr(1)) !== -1
+
+    if (tipoEhValido) {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+        fs.createReadStream(caminho)
+        .pipe(fs.createWriteStream(novoCaminho))
+        .on('finish', () => callbackImagemCriada(false, novoCaminho))
+    } else {
+        const erro = 'Tipo é inválido!'
+        console.log('Erro! Tipo inválido!')
+        callbackImagemCriada(erro)
+    }
+}
